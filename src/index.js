dca84790348c3ad2b2dd3ed1d31bad699426d816
Yahya Moulida <yahyam11111@gmail.com>
2025-09-17 15:50:18 +0100

    cron.js added

--- src/index.js
+++ src/index.js
@@ -4,16 +4,17 @@ import authRoutes from "./routes/authRoutes.js";
 import { connectDB } from "./lib/db.js";
 import bookRoutes from "./routes/bookRoutes.js";
 import cors from "cors";
+import job from "./lib/cron.js";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
+job.start();
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
-
-const PORT = process.env.PORT;
 
 app.listen(3000, () => {
     console.log(`server are running on PORT : ${PORT}`);
